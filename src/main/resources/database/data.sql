8efb4cdc581ba2b815261ff9e4b4810d41339619
Manoj Garimella <narasimhamanoj27@gmail.com>
2020-07-02 17:29:29 +0530

    Fix for H2 database config

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO ORDER VALUES (1, 'Manoj', 19/06/2020, 'Miyapur' 10.02);
-INSERT INTO ORDER VALUES (2, 'Manoj1', 20/06/2020, 'Kukatpally', 11.01);
-INSERT INTO ORDER VALUES (3, 'Manoj2', 21/06/2020, 'Kothapet', 13.08);
+INSERT INTO ORDER VALUES (1, 'Manoj', {ts '2013-09-17 18:47:52.69'}, 'Miyapur' 10.02);
+INSERT INTO ORDER VALUES (2, 'Manoj1', {ts '2014-09-17 18:47:52.69'}, 'Kukatpally', 11.01);
+INSERT INTO ORDER VALUES (3, 'Manoj2', {ts '2015-09-17 18:47:52.69'}, 'Kothapet', 13.08);
