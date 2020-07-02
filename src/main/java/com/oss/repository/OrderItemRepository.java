4a1dd9c2e894ab35f0d60d2864dc730b0b8e5444
Manoj Garimella <narasimhamanoj27@gmail.com>
2020-07-02 17:44:19 +0530

    Changes for H2 configuration

--- src/main/java/com/oss/repository/OrderItemRepository.java
+++ src/main/java/com/oss/repository/OrderItemRepository.java
@@ -1,9 +1,11 @@
 package com.oss.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.oss.api.OrderItem;
 
+@Repository
 public interface OrderItemRepository extends CrudRepository<OrderItem, Integer>{
 
 }
--- src/main/java/com/oss/repository/OrderRepository.java
+++ src/main/java/com/oss/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.oss.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.oss.api.Orders;
 
+@Repository
 public interface OrderRepository extends CrudRepository<Orders, Integer>{
 
 }
